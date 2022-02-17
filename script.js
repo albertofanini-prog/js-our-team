4b015cb33f1e67b32bd815f75813087a4a7f13c7
albertofanini-prog <albertofanini@outlook.com>
2022-02-17 16:49:03 +0100

    2nd Commit - Spiegazione in italiano

---
+++ script.js
@@ -0,0 +1,5 @@
+//Viene fornito un layout di base
+    //Ogni membro ha le info necessarie: nome, ruolo e foto
+
+//Usare come riferimento il codice html della card presente
+//Stampare una card per ogni membro
